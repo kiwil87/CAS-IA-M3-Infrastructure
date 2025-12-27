098be1a2fe0f08d11885d9b1b3c0cb3167ab4103
kiwil87 <lev.kiwi@gmail.com>
2025-12-27 13:24:29 +0000

    new connection strings

--- 01-Introduction/03_Connecting_to_datasource_PySpark.py
+++ 01-Introduction/03_Connecting_to_datasource_PySpark.py
@@ -1,13 +1,13 @@
 # Databricks notebook source
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName("reading sql database").getOrCreate()
-jdbcHostname = "sql-datasource-dev-001.database.windows.net"
-jdbcDatabase = "sqldb-adventureworks-dev-001"
+jdbcHostname = "sql-kil-datasource-prod-001.database.windows.net"
+jdbcDatabase = "sqldb-kil-adventureworks-prod-001"
 jdbcPort = 1433
 jdbcUrl = "jdbc:sqlserver://{0}:{1};database={2}".format(jdbcHostname, jdbcPort, jdbcDatabase)
 connectionProperties = {
-    "user" : "levkiwi-admin",
-    "password" : "cas-ia2024",
+    "user" : "YOUR_USERNAME",
+    "password" : "YOUR_PASSWORD",
     "driver" : "com.microsoft.sqlserver.jdbc.SQLServerDriver"
 }
 
